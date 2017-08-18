59786988242d4b2896c3ded7235d921a44ec9364
Danijar Hafner <mail@danijar.com>
2017-08-18 11:13:47 -0700

    Initial release.

---
+++ setup.py
@@ -0,0 +1,46 @@
+# Copyright 2017 The TensorFlow Agents Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Setup script for TensorFlow Agents."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import setuptools
+
+
+setuptools.setup(
+    name='agents',
+    version='1.0.0',
+    description=(
+        'Efficient TensorFlow implementation of reinforcement learning '
+        'algorithms.'),
+    license='Apache 2.0',
+    url='http://github.com/tensorflow/agents',
+    install_requires=[
+        'tensorflow',
+        'gym',
+        'ruamel.yaml',
+    ],
+    packages=[
+        'agents',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Intended Audience :: Science/Research',
+    ],
+)
