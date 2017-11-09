e6737cd8e04836a6024f33f2c07d54bc3ccf6485
Jeremy Lewi <jeremy@lewi.us>
2017-11-09 05:02:04 -0800

    Fix a typo in the docstring of simulate. (#16)

--- agents/tools/simulate.py
+++ agents/tools/simulate.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""In-graph simulation step of a vecrotized algorithm with environments."""
+"""In-graph simulation step of a vectorized algorithm with environments."""
 
 from __future__ import absolute_import
 from __future__ import division
